b799f1c8478034bbc75d4bbf914c21da1a1896ca
LoseAngel <32516453+LoseAngel@users.noreply.github.com>
2022-05-21 22:40:54 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="138.2.114.202.sslip.io";
+        url.hostname="kr1.forgot.ml";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
